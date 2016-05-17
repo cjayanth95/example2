8ba207d38ce48e803e21c1668587be6d3e70cdae
labuser <labuser@mgaba-mmini-2.corp.adobe.com>
2016-05-17 11:39:54 +0530

    Initial Commit

---
+++ example2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example2
+//
+//  Created by labuser on 17/05/16.
+//  Copyright © 2016 labuser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
