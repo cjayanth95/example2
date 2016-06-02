a95d065a6caf07a840bb6a00e729123f96161d19
Adobe <Adobe@una.macromedia.com>
2016-06-02 15:59:15 +0530

    Working till images separation

--- example2/main.cpp
+++ example2/main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 using namespace cv;
 std::vector<cv::Rect> detectLetters(cv::Mat img , int x , int y,  int s)
 {
+//    Mat bwImage;
+//    cv::cvtColor(img, bwImage, CV_RGB2GRAY);
     std::vector<cv::Rect> boundRect;
     cv::Mat img_gray, img_sobel, img_threshold, element;
     cvtColor(img, img_gray, CV_BGR2GRAY);
@@ -19,9 +21,13 @@ std::vector<cv::Rect> detectLetters(cv::Mat img , int x , int y,  int s)
     cv::morphologyEx(img_threshold, img_threshold, CV_MOP_CLOSE, element); //Does the trick
     std::vector< std::vector< cv::Point> > contours;
     cv::findContours(img_threshold, contours, 0, 1);
+    //vector<Vec4i> hierarchy;
+    
+    //cv::findContours( bwImage, contours, hierarchy,CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
     std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
     for( int i = 0; i < contours.size(); i++ )
-        if (contours[i].size()>s)
+        //if (contours[i].area()>s)
+         if ((contours[i]).size()>s)
         {
             cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
             cv::Rect appRect( boundingRect( cv::Mat(contours_poly[i]) ));
@@ -34,15 +40,20 @@ std::vector<cv::Rect> detectLetters(cv::Mat img , int x , int y,  int s)
 int main(int argc,char** argv)
 {
     //Read
-    cv::Mat img1=cv::imread("88.jpg");
-    cv::Mat img2=cv::imread("88.jpg");
-    cv::Mat img3= img1.clone();
+    using namespace cv;
+    cv::Mat img1=cv::imread("15.jpg");
+    cv::Mat img2=cv::imread("15.jpg");
+//    Mat inv;
+//    threshold(img1,inv,254,255,THRESH_BINARY_INV);
+//    cv::imwrite( "inv.jpg", inv);
+//    cv::Mat img3= img1.clone();
+//    img3.setTo(255);
     //Detect
     std::vector<cv::Rect> letterBBoxes1=detectLetters(img1 ,17 , 3 ,100);
-    std::vector<cv::Rect> letterBBoxes2=detectLetters(img2, 350 ,50,100);
-    std::vector<cv::Rect> letterBBoxes3=detectLetters(img3(letterBBoxes1[2]) , 21, 3,300);
-    cv::Mat img4=img3(letterBBoxes1[2]);
-    cout<<letterBBoxes1[2].area()<<endl;
+    std::vector<cv::Rect> letterBBoxes2=detectLetters(img1, 140,20,100);
+    //std::vector<cv::Rect> letterBBoxes3=detectLetters(img3(letterBBoxes1[2]) , 21, 3,300);
+    //cv::Mat img4=img3(letterBBoxes1[2]);
+    //cout<<letterBBoxes1[2].area()<<endl;
     //cout<<letterBBoxes3[0].area()<<endl;
     //double ratio= letterBBoxes1[0].area()/letterBBoxes3[0].area();
 //            namedWindow( "Contours", WINDOW_AUTOSIZE );
@@ -53,16 +64,17 @@ int main(int argc,char** argv)
     //cout<<ratio<<endl;
     for(int i=0; i< letterBBoxes1.size(); i++){
         img1(letterBBoxes1[i] ).setTo(255);
+        //img3(letterBBoxes1[i] ).setTo(0);
         //cvSet(img1(letterBBoxes1[i] ), CV_RGB(0,0,0));
     }
     
     cv::imwrite( "imgOut4.jpg", img1);
     for(int i=0; i< letterBBoxes2.size(); i++)
-        cv::rectangle(img2,letterBBoxes2[i],cv::Scalar(0,255,0),3,8,0);
-    cv::imwrite( "imgOut5.jpg", img2);
-    for(int i=0; i< letterBBoxes3.size(); i++)
-        cv::rectangle(img4,letterBBoxes3[i],cv::Scalar(0,0,255),3,8,0);
-    cv::imwrite( "imgOut6.jpg", img4);
+        cv::rectangle(img2,letterBBoxes2[i],cv::Scalar(255,255,0),3,8,0);
+    cv::imwrite( "imgOut10.jpg", img2);
+//    for(int i=0; i< letterBBoxes3.size(); i++)
+//        cv::rectangle(img4,letterBBoxes3[i],cv::Scalar(0,0,255),3,8,0);
+//    cv::imwrite( "imgOut6.jpg", img4);
     return 0;
 }
 
